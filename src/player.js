b81e8e6d2c885b20f3b7b867257289a16c59b8d9
Sagun <120552108+Sagun15@users.noreply.github.com>
2025-06-30 16:42:14 +0530

    Update player.js

--- src/player.js
+++ src/player.js
@@ -2,7 +2,7 @@ export class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 80;
+    this.width = 160;
     this.height = 20;
     this.speed = 6;
     this.dx = 0;
