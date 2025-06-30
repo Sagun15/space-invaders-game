47b15532de758a0c0226ddd6bdbdf6edb3fa083f
Sagun <120552108+Sagun15@users.noreply.github.com>
2025-06-30 16:57:04 +0530

    Auto-sync from Bolt - 2025-06-30T11:27:02.044Z

--- src/player.js
+++ src/player.js
@@ -2,7 +2,7 @@ export class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 40;
+    this.width = 240;
     this.height = 20;
     this.speed = 5;
     this.dx = 0;
